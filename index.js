0bbc9ab1ceb1e8ea52678b61c344483b51314f1c
Tommy M. McGuire <tommy.mcguire@nasa.gov>
2020-12-02 16:27:58 -0600

    Day 2: 3-4

--- index.js
+++ index.js
@@ -29,4 +29,53 @@ const fs = require('fs');
 
 // ====================================
 
-const input = fs.readFileSync('inputs/3').toString();
+// function count(ch, str) {
+//   // let c = 0;
+//   // for (let i = 0; i < str.length; ++i) {
+//   //   if (str[i] === ch) { c += 1; }
+//   // }
+//   // return c;
+//   const chs = [...str].filter(l => l === ch);
+//   return chs.length;
+//   // const chs = Array.from(str).filter(l => l === ch);
+//   // return chs.length;
+// }
+
+// function isvalid(p) {
+//   const cnt = count(p.lett, p.pass);
+//   return p.min <= cnt && cnt <= p.max;
+// }
+
+// function isvalid2(p) {
+//   let a = p.min - 1;
+//   let b = p.max - 1;
+//   return (
+//     p.pass.length >= a && p.pass.length >= b
+//     && (
+//       (p.pass[a] === p.lett && p.pass[b] !== p.lett)
+//       || (p.pass[a] !== p.lett && p.pass[b] === p.lett)
+//     ))
+// }
+
+// function main() {
+//   const validInput =
+//     fs.readFileSync('inputs/3')
+//       .toString()
+//       .split('\n')
+//       .filter(ln => ln.length > 0)
+//       .map(ln => {
+//         let flds = ln.split(':');
+//         let policy = flds[0].split(' ');
+//         let minmax = policy[0].split('-');
+//         return {
+//           min: Number.parseInt(minmax[0], 10),
+//           max: Number.parseInt(minmax[1], 10),
+//           lett: policy[1],
+//           pass: flds[1].trim(),
+//         };
+//       })
+//       .filter(isvalid2);
+//   console.log(validInput.length);
+// }
+
+main();
